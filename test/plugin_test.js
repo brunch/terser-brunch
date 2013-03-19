79bfe57419e81444be161d477eb36779f9be1da5
Paul Miller <paul@paulmillr.com>
2013-03-19 07:56:11 +0200

    Fix tests.

--- test/plugin_test.js
+++ test/plugin_test.js
@@ -9,15 +9,15 @@ describe('Plugin', function() {
     expect(plugin).to.be.ok;
   });
 
-  it('should has #minify method', function() {
-    expect(plugin.minify).to.be.an.instanceof(Function);
+  it('should has #optimize method', function() {
+    expect(plugin.optimize).to.be.an.instanceof(Function);
   });
 
   it('should compile and produce valid result', function(done) {
     var content = '(function() {var first = 5; window.second = first;})()';
     var expected = '(function(){var n=5;window.second=n})();';
 
-    plugin.minify(content, '', function(error, data) {
+    plugin.optimize(content, '', function(error, data) {
       expect(error).not.to.be.ok;
       expect(data).to.equal(expected);
       done();
