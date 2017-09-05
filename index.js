ba073c59f17d8bbe23654ac003870ec8dccbc8d9
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-09-06 00:49:36 +0300

    update code

--- index.js
+++ index.js
@@ -1,61 +1,48 @@
 'use strict';
-
 const uglify = require('uglify-js');
+const anymatch = require('anymatch');
 
-const formatError = (error) => {
-  const err = new Error(`L${error.line}:${error.col} ${error.message}`);
+const formatError = old => {
+  const err = new Error(`L${old.line}:${old.col} ${old.message}`);
   err.name = '';
-  err.stack = error.stack;
+  err.stack = old.stack;
   return err;
 };
 
 class UglifyJSOptimizer {
   constructor(config) {
-    this.options = Object.assign({}, config.plugins.uglify);
-    this.options.fromString = true;
-    this.options.sourceMaps = !!config.sourceMaps;
+    this.options = Object.assign({
+      sourceMap: !!config.sourceMaps,
+    }, config.plugins.uglify);
+
+    this.ignored = anymatch(this.options.ignored);
+    delete this.options.ignored;
   }
 
   optimize(file) {
-    const data = file.data;
-    const path = file.path;
-
-    try {
-      if (this.options.ignored && this.options.ignored.test(file.path)) {
-        // ignored file path: return non minified
-        const result = {
-          data,
-          // brunch passes in a SourceMapGenerator object, but wants a string back.
-          map: file.map ? file.map.toString() : null,
-        };
-        return Promise.resolve(result);
-      }
-    } catch (e) {
-      return Promise.reject(`error checking ignored files to uglify ${e}`);
-    }
-
-    if (file.map) {
-      this.options.inSourceMap = file.map.toJSON();
-    }
-
-    this.options.outSourceMap = this.options.sourceMaps ?
-      `${path}.map` : undefined;
-
-    try {
-      const optimized = uglify.minify(data, this.options);
-
-      const result = optimized && this.options.sourceMaps ? {
-        data: optimized.code,
-        map: optimized.map,
-      } : {
-        data: optimized.code,
+    if (this.ignored(file.path)) {
+      return {
+        data: file.data,
+        map: file.map && `${file.map}`,
       };
-      result.data = result.data.replace(/\n\/\/# sourceMappingURL=\S+$/, '');
-
-      return Promise.resolve(result);
-    } catch (err) {
-      return Promise.reject(formatError(err));
     }
+
+    const options = Object.assign({}, this.options);
+    if (file.map) {
+      options.sourceMap = {
+        content: JSON.stringify(file.map),
+        url: `${file.path}.map`,
+      };
+    }
+
+    const res = uglify.minify(file.data, options);
+    if (res.error) throw formatError(res.error);
+    if (!res.map) return {data: res.code};
+
+    return {
+      data: res.code.replace(/\/\/# sourceMappingURL=\S+$/, ''),
+      map: res.map,
+    };
   }
 }
 
