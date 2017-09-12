9ebedb98dd258cf90aeff410691ea349e5fa95c4
Aleksey Shvayka <shvaikalesh@gmail.com>
2017-09-12 14:58:39 +0300

    add modern js test

--- test.js
+++ test.js
@@ -3,8 +3,40 @@
 require('chai/register-should');
 const Plugin = require('.');
 
+const path = 'file.js';
 const data = '(function() {var first = 5; window.second = first;})()';
 const uglified = 'window.second=5;';
+const modern = `
+  class ಠ_ಠ extends Array {
+    constructor(j = "a", ...c) {
+      const q = (({u: e}) => {
+        return { [\`s\${c}\`]: Symbol(j) };
+      })({});
+
+      super(j, q, ...c);
+    }
+  }
+
+  new Promise((f) => {
+    const a = function* (){
+      return "\u{20BB7}".match(/./u)[0].length === 2 || true;
+    };
+
+    for (let vre of a()) {
+      const [uw, as, he, re] = [
+        new Set(),
+        new WeakSet(),
+        new Map(),
+        new WeakMap(),
+      ];
+      break;
+    }
+
+    f(new Proxy({}, {
+      get: (han, h) => h in han ? han[h] : "42".repeat(0o10)
+    }),);
+  }).then(bi => new ಠ_ಠ(bi.rd));
+`;
 
 describe('uglify-js-brunch', () => {
   let plugin;
@@ -20,8 +52,19 @@ describe('uglify-js-brunch', () => {
   });
 
   it('should compile and produce valid result', () => {
-    plugin.optimize({data, path: 'file.js'})
-      .should.eql({data: uglified});
+    plugin.optimize({data, path}).should.eql({data: uglified});
+  });
+
+  it('should optimize modern JavaScript', () => {
+    const code = plugin.optimize({data: modern, path}).data;
+
+    // eslint-disable-next-line no-eval
+    return eval(code).then(res => {
+      res.should.be.an('array');
+
+      res[0].should.equal('4242424242424242');
+      res[1].s.should.be.a('symbol');
+    });
   });
 
   it('should produce source maps', () => {
@@ -30,7 +73,7 @@ describe('uglify-js-brunch', () => {
       plugins: {},
     });
 
-    plugin.optimize({data, path: 'file.js'}).should.eql({
+    plugin.optimize({data, path}).should.eql({
       data: uglified,
       map: '{"version":3,"sources":["0"],"names":["window","second"],"mappings":"AAA4BA,OAAOC,OAAV"}',
     });
